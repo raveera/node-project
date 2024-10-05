3f1b31cc55ee7736e169c658655bf9a6723a57e0
Danish <raveera39@hotmail.com>
2024-10-06 04:03:35 +0700

    refactor(route) change name

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const user = require('./route/user')
+const userRoute = require('./route/user-route')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,6 +9,6 @@ app.use(cors())
 app.use(express.json())
 
 // router
-app.use('/user', user)
+app.use('/user', userRoute)
 
 app.listen(port, () => console.log('Node.js Port => ', port))
